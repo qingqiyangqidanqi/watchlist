efd9afa1ab32e0cb5f56ebd6b3216d15eba30681
jiesheng-home <jiesheng200103@gmail.com>
2024-07-03 00:29:02 +0800

    Add database support with flask-SQLAlichemy

--- app.py
+++ app.py
@@ -1,27 +1,95 @@
+import os
+import sys
+import click
+
 from flask import Flask, url_for, render_template
 from markupsafe import escape  # 用户输入的数据会包含恶意代码，所以不能直接作为响应返回
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:  # if window system, use '///'
+    prefix = 'sqlite:///'
+else:  # else use '////'
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+'''
+模型类的编写有一些限制：
+    模型类要声明继承 db.Model。
+    每一个类属性（字段）要实例化 db.Column，传入的参数为字段的类型。
+    在 db.Column() 中添加额外的选项（参数）可以对字段进行设置。比如，primary_key 设置当前字段是否为主键。除此之外，常用的选项还有 nullable（布尔值，是否允许为空值）、index（布尔值，是否设置索引）、unique（布尔值，是否允许重复值）、default（设置默认值）等。
+'''
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+
+'''
+终端执行 flask initdb 命令就可以创建数据库表
+终端执行 flask initdb --drop 命令可以删除表后重新创建
+'''
+
+
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()  # 删除数据库
+    db.create_all()  # 创建数据库
+    click.echo('Initialized database.')  # 输出提示信息
+
+
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
 
 
 @app.route('/')
 def index():  # put application's code here
+    user = User.query.first()   # read data for user
+    movies = Movie.query.all()  # read data for movie
     # return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-    return render_template('index.html', name=name, movies=movies)
+    return render_template('index.html', user=user, movies=movies)
 
 
 @app.route('/user/<name>')
@@ -43,5 +111,7 @@ def test_url_for():
 
 
 if __name__ == '__main__':
+    # db.drop_all()     # 创建数据库
+    # db.create_all()   # 删除数据库
     app.debug = True
     app.run()
---
+++ db_test.py
@@ -0,0 +1,41 @@
+from app import db, User, Movie  # 导入模型类
+
+
+def create():
+    # 创建
+    user = User(name='Grey Li')  # 创建一个 User 记录
+    m1 = Movie(title='Leon', year='1994')  # 创建一个 Movie 记录
+    m2 = Movie(title='Mahjong', year='1996')  # 再创建一个 Movie 记录
+    db.session.add(user)  # 把新创建的记录添加到数据库会话
+    db.session.add(m1)
+    db.session.add(m2)
+    db.session.commit()  # 提交数据库会话，只需要在最后调用一次即可
+
+
+def query():
+    # 读取 查询语句：<模型类>.query.<过滤方法（可选）>.<查询方法>
+    movie = Movie.query.first()  # 获取 Movie 模型的第一个记录（返回模型类实例）
+    print(movie.title)  # 对返回的模型类实例调用属性即可获取记录的各字段数据
+    print(movie.year)
+    print(Movie.query.all())  # 获取 Movie 模型的所有记录，返回包含多个模型类实例的列表
+    print(Movie.query.count())  # 获取 Movie 模型所有记录的数量
+    print(Movie.query.get(1))  # 获取主键值为 1 的记录
+    print(Movie.query.filter_by(title='Mahjong').first())  # 获取 title 字段值为 Mahjong 的记录
+    print(Movie.query.filter(Movie.title == 'Mahjong').first())  # 等同于上面的查询，但使用不同的过滤方法
+
+
+def updata():
+    # 下面的操作更新了 Movie 模型中主键为 2 的记录
+    movie = Movie.query.get(2)
+    movie.title = 'WALL-E'
+    movie.year = '2008'
+    db.session.commit()
+
+def delete():
+    # 下面的操作删除了 Movie 模型中主键为 1 的记录
+    movie = Movie.query.get(1)
+    db.session.delete(movie)
+    db.session.commit()
+
+if __name__ == '__main__':
+    pass
